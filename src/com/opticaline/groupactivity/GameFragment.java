2521865d080b9e7d7322ca7555eac694353b8f60
bro_ <365492386@qq.com>
2013-11-21 09:12:40 +0800

    Add GameFragment

---
+++ src/com/opticaline/groupactivity/GameFragment.java
@@ -0,0 +1,10 @@
+package com.opticaline.groupactivity;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 13-11-21.
+ */
+public class GameFragment extends Fragment {
+
+}
